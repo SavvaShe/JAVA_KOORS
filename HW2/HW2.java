3ecb37e2d12650809a1e797debf0f893b10de97e
SavvaShe <79318479+SavvaShe@users.noreply.github.com>
2022-03-10 14:51:18 +0300

    Add files via upload

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -6,60 +6,47 @@ public class HW2 {
 
 
     //1-2
-//    private  static  void mas(){
-//        int sumstr=0;
-//        int sumst=0;
-//        for(int i = 0;i < 5;i++){
-//            for(int j = 0;j < 5;j++){
-//                System.out.println("Сумма строки = "+sumstr);
-//                System.out.print((i * j) + "\t");
-//                sumstr += j;
-//
-//            }
-//            sumst += i;
-//
-//            System.out.println();
-//            System.out.println("Сумма строки = "+sumst);
-//        }
-//
-//
-//    }
-    //3
-    private  static void massR(){
+    private  static  void mas(){
 
-//        String[] sA = {"слова", "которые", "не", "просто", "слова", "а", "массив", "слов"};
-//        String[] sB = new String[sA.length];
-//        HashMap<String, String> shMap  = new HashMap<String, String>();
-//
-//        for (int i = 0; i < sA.length; i++) shMap.put(sA[i], "");
-//        int j=0;
-//        for (int i = 0; i < sA.length; i++) {
-//            if( shMap.get(sA[i]).equals("")) {
-//                shMap.put(sA[i], sA[i]);
-//                sB[j++] = sA[i];
-//            }
-//        }
-//        System.out.println(sA[i] + " ");
-//        System.out.println(sB[i] + " ");
+        int sumst=0;
+        for(int i = 0;i < 5;i++){
+            int sumstr = 0;
+            for(int j = 0;j < 5;j++){
+                int sumstr1=0;
+                System.out.print((i * j) + "\t");
+                sumstr1 += (i*j);
+
+                sumstr += sumstr1;
+            }
+
+            System.out.println("Сумма строки = "+sumstr);
+            System.out.println("Сумма строки = "+sumstr);
+            System.out.println();
+
+
+        }
+
 
     }
+    //3
+
 
         public static  void main(String[] args)
-        {//        mas();
+        {        mas();
 
 
-            int[] numbers = new int[15];
-            for(int i = 0;i < numbers.length;i++){
-                numbers[i] = (int)(Math.random()*100);
-                System.out.println(numbers[i]);
-
-            }
-            System.out.println("___________________________________");
-            int[] uniqueNumbers = Arrays.stream(numbers).distinct().toArray();
-            for (int i : uniqueNumbers)
-            {
-                System.out.println(i);
-            }
+//            int[] numbers = new int[15];
+//            for(int i = 0;i < numbers.length;i++){
+//                numbers[i] = (int)(Math.random()*100);
+//                System.out.println(numbers[i]);
+//
+//            }
+//            System.out.println("___________________________________");
+//            int[] uniqueNumbers = Arrays.stream(numbers).distinct().toArray();
+//            for (int i : uniqueNumbers)
+//            {
+//                System.out.println(i);
+//            }
         }
 
 
